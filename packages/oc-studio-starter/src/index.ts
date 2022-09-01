c36633ecc4c964d17b13c89b239c208e167ede0c
Michael Myers <michael.morris.myers@gmail.com>
2022-09-01 17:25:05 -0400

    chore:done

--- packages/oc-studio-starter/src/index.ts
+++ packages/oc-studio-starter/src/index.ts
@@ -3,8 +3,11 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require("dotenv").config();
 
+// Lambda
+import { Callback, Context } from "aws-lambda";
+
 // Assistant
-import { Assistant } from "stentor";
+import { Assistant, setEnv } from "stentor";
 
 // Channels
 import { Alexa } from "@xapp/stentor-alexa";
@@ -23,26 +26,32 @@ import { StudioService } from "stentor-service-studio";
 // Custom Handlers
 import { QuestionAnsweringHandler } from "@xapp/question-answering-handler";
 
-// Leverage external NLU
-const nlu = new LexServiceV2({
-    botId: process.env.LEX_BOT_ID,
-    botAlias: process.env.LEX_BOT_ALIAS
-});
+export async function handler(event: any, context: Context, callback: Callback<any>): Promise<void> {
+    await setEnv().then().catch((error: Error) => console.error("Environment failed to load", error));
 
-const studioService: StudioService = new StudioService({ appId: process.env.STUDIO_APP_ID, token: process.env.STUDIO_TOKEN });
+    // Leverage external NLU
+    const nlu = new LexServiceV2({
+        botId: process.env.LEX_BOT_ID,
+        botAlias: process.env.LEX_BOT_ALIAS
+    });
 
-// Return the handler for running in an AWS Lambda function.
-export const handler = new Assistant()
-    .withUserStorage(new DynamoUserStorage())
-    .withHandlerService(studioService)
-    .withKnowledgeBaseService(studioService, {
-        // Intent ID for your fallback to determine if we call  KnowledgeBase
-        matchIntentId: "InputUnknown",
-        // For KnowledgeBase results we will generate a request with the following ID
-        setIntentId: "OCSearch"
-    })
-    .withHandlers({
-        QuestionAnsweringHandler: QuestionAnsweringHandler
-    })
-    .withChannels([Alexa(), Dialogflow(), LexConnect(), LexV2Channel(), Stentor(nlu)])
-    .lambda();
+    const studioService: StudioService = new StudioService({ appId: process.env.STUDIO_APP_ID, token: process.env.STUDIO_TOKEN });
+
+    // Return the handler for running in an AWS Lambda function.
+    const assistant = new Assistant()
+        .withUserStorage(new DynamoUserStorage())
+        .withHandlerService(studioService)
+        .withKnowledgeBaseService(studioService, {
+            // Intent ID for your fallback to determine if we call  KnowledgeBase
+            matchIntentId: "InputUnknown",
+            // For KnowledgeBase results we will generate a request with the following ID
+            setIntentId: "OCSearch"
+        })
+        .withHandlers({
+            QuestionAnsweringHandler: QuestionAnsweringHandler
+        })
+        .withChannels([Alexa(), Dialogflow(), LexConnect(), LexV2Channel(), Stentor(nlu)])
+        .lambda();
+
+    await assistant(event, context, callback);
+}
