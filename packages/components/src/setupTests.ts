c21b5eb75860235b4b2f6ccd1814ec98d2aa0b4b
Michael Myers <michael.morris.myers@gmail.com>
2022-09-22 13:14:27 -0400

    chore:fix lint

--- packages/components/src/setupTests.ts
+++ packages/components/src/setupTests.ts
@@ -1,3 +1,4 @@
+/*! Copyright (c) 2022, XAPP AI */
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
